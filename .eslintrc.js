7c648443d89f0318d9a9ee7f76404c0ec60552c0
Lerc <lerc@screamingduck.com>
2019-04-23 07:55:14 +1200

    jsdoc and lint config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        log: "readonly",print: "readonly",clear: "readonly",makeBackground: "readonly",setColour: "readonly",setColor: "readonly",setRgb: "readonly",setGrey: "readonly",drawLine: "readonly",
+        drawCircle: "readonly",drawRectangle: "readonly",drawPolygon: "readonly",drawShape: "readonly",drawStar: "readonly",
+        fillCircle: "readonly",fillRectangle: "readonly",fillPolygon: "readonly",fillShape: "readonly",fillStar: "readonly",
+        loadImage: "readonly",drawImage: "readonly",drawImageRect: "readonly",
+        canvasSave: "readonly",canvasRestore: "readonly",canvasTransform: "readonly",canvasTranslate: "readonly",canvasRotate: "readonly",canvasScale: "readonly",
+        keyIsDown: "readonly",keyWentDown: "readonly",getMousePosition: "readonly",getMouseInfo: "readonly",
+        distance: "readonly",rgb: "readonly",grey: "readonly",random: "readonly",stringify: "readonly",noise2d: "readonly",noise3d: "readonly",
+        run: "readonly",makeCanvas: "readonly",makeFullPageCanvas:"readOnly"
+
+    },
+
+    "rules": {
+    }
+
+};
